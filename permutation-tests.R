7bb8c0331e7cfa127c9113edf84c84621ff0daf6
Logan <logancestell@gmail.com>
2025-10-10 13:27:41 -0700

    commit

--- permutation-tests.R
+++ permutation-tests.R
@@ -36,7 +36,7 @@ if (force_perms | !file.exists(perm_test_file))  {
     zoo_name <- tolower(x= gsub(pattern = " ",
                                 replacement = "_",
                                 x = zoos$name[zoo_i]))
-    zoo_file <- paste0("data/gbif/", zoo_name, "-obs.csv")
+    zoo_file <- paste0("Data/GBIF/", zoo_name, "-obs.csv")
     zoo_obs <- read.csv(file = zoo_file)
     #only keep records with species name and year >= min_year
     zoo_obs <- zoo_obs %>%
@@ -61,7 +61,7 @@ if (force_perms | !file.exists(perm_test_file))  {
     # Only proceed if number of zoo observations is large enough
     if (nrow(zoo_obs) >= min_obs) {
       # RICHNESS for this garden
-      garden_richness <- length(unique(zoo_obs$species))
+      zoo_richness <- length(unique(zoo_obs$species))
       
       # DIVERSITY for this zoo (Shannon's Index, H)
       zoo_diversity <- zoo_obs %>%
@@ -196,6 +196,241 @@ if (force_perms | !file.exists(perm_test_file))  {
                                         # upper_95 = upper_95,
                                         richness_quantile = richness_quantile,
                                         diversity_quantile = diversity_quantile)
-      #START AT LINE 213!
+ #Permutation t-tests, we bootstrap zoo and city obs, creating a random sample
+#of 20% of zoo obs to test if city richness and diversity are higher than zoo richness and diversity
+      #restrict to only zoos with >= 50 obs
+      
+    if (nrow(zoo_obs) >= 50) {
+      message(paste0("Running permutation t-test for ", zoos$name[zoo_i]))
+      sample_size <- ceiling(x = 0.2 * nrow(zoo_obs))
+      
+      richness_mat <- matrix(data = NA, nrow = nreps, ncol = 2)
+      diversity_mat <- matrix(data = NA, nrow = nreps, ncol = 2)
+      for (rep_i in 1:nreps) {      
+        zoo_sample <- zoo_obs %>%
+          slice_sample(n = sample_size)
+        
+        city_sample <- city_obs %>%
+          slice_sample(n = sample_size)
+        
+        # zoo richness in column 1, city richness in column 2
+        richness_mat[rep_i, 1] <- length(unique(zoo_sample$species))
+        richness_mat[rep_i, 2] <- length(unique(city_sample$species))
+        
+        # zoo diversity in column 1, city diversity in column 2
+        diversity_mat[rep_i, 1] <- zoo_sample %>%
+          group_by(species) %>%
+          summarize(abun = n()) %>%
+          ungroup() %>%
+          mutate(total_abun = sum(abun)) %>%
+          mutate(p = abun/total_abun) %>%
+          mutate(plogp = -1 * (p * log(p))) %>%
+          select(plogp) %>%
+          sum()
+        
+        diversity_mat[rep_i, 2] <- city_sample %>%
+          group_by(species) %>%
+          summarize(abun = n()) %>%
+          ungroup() %>%
+          mutate(total_abun = sum(abun)) %>%
+          mutate(p = abun/total_abun) %>%
+          mutate(plogp = -1 * (p * log(p))) %>%
+          select(plogp) %>%
+          sum()
+      }
+      # Run t-test on resulting matrix; is zoo richness significantly lower 
+      # than city?
+      richness_t <- t.test(x = richness_mat[, 1], 
+                           y = richness_mat[, 2],
+                           alternative = "less")
+      diversity_t <- t.test(x = diversity_mat[, 1],
+                            y = diversity_mat[, 2],
+                            alternative = "less")
+      
+      perm_tests[[zoo_name]][["richness_t"]] <- richness_t
+      perm_tests[[zoo_name]][["diversity_t"]] <- diversity_t
+    } else {
+      message(paste0("Skipping permutation t-test for ", zoos$name[zoo_i],
+                     ", too few observations"))
+    }
+    
+    
+    
+  } else {
+    message("Too few observations in ", zoos$name[zoo_i], 
+            " no permutation tests performed.")
   }
+}
+saveRDS(object = perm_tests,
+        file = perm_test_file)
+} else {
+  message("Permutation test results already on file. Using those.")
+}
+
+# Extract values from the list of permutations, putting them in one data frame 
+# for ease of plotting. The garden column gets lots of mutates to make plot 
+# a little nicer
+
+if (file.exists(perm_test_file)) {
+  perm_tests <- readRDS(file = perm_test_file)
+} else {
+  message("No perm test results file on disk")
+}
+sample_values_list <- lapply(X = perm_tests, FUN = "[[", "sample_values")
+sample_values <- dplyr::bind_rows(sample_values_list, .id = "zoo")
+sample_values <- sample_values %>%
+  rename(zoo_name = zoo) %>%
+  mutate(zoo_print = gsub(pattern = "_",
+                             replacement = " ",
+                             x = zoo_name)) %>%
+  mutate(zoo_print = stringr::str_to_title(zoo_print)) %>%
+  mutate(zoo_print = gsub(pattern = "Abq",
+                             replacement = "ABQ",
+                             x = zoo_print)) %>%
+  mutate(zoo_print = gsub(pattern = "San Antonio", # To fit on plot
+                             replacement = "\nSan Antonio",
+                             x = zoo_print)) %>%
+  mutate(zoo_print = gsub(pattern = "Reid Park",
+                             replacement = "\nReid Park",
+                             x = zoo_print)) %>%
+  mutate(zoo_print = gsub(pattern = "Phx",
+                             replacement = "\nPheonix",
+                             x = zoo_print))
+  mutate(zoo_print = gsub(pattern = "San Diego",
+                        replacement = "\nSan Diego",
+                        x = zoo_print))
+  mutate(zoo_print = gsub(pattern = "Los Angeles",
+                        replacement = "\nLos Angeles",
+                        x = zoo_print))
+
+
+# Pull out values for each zoo; similar mutations in zoo name for plots
+zoo_richness <- unlist(lapply(X = perm_tests, 
+                                 FUN = "[[", "zoo_richness"))
+zoo_diversity <- unlist(lapply(X = perm_tests, 
+                                  FUN = "[[", "zoo_diversity"))
+richness_quantiles <- unlist(lapply(X = perm_tests, 
+                                    FUN = "[[", "richness_quantile"))
+diversity_quantiles <- unlist(lapply(X = perm_tests, 
+                                     FUN = "[[", "diversity_quantile"))
+zoo_values <- data.frame(zoo = names(zoo_richness),
+                            richness = zoo_richness,
+                            richness_quantile = richness_quantiles,
+                            diversity = zoo_diversity,
+                            diversity_quantile = diversity_quantiles)
+rownames(zoo_values) <- NULL
+
+
+# Updates to zoo names for printing purposes
+zoo_values <- zoo_values  %>%
+  rename(zoo_name = zoo) %>%
+  mutate(zoo_print = gsub(pattern = "_",
+                             replacement = " ",
+                             x = zoo_name)) %>%
+  mutate(zoo_print = stringr::str_to_title(zoo_print)) %>%
+  mutate(zoo_print = gsub(pattern = "Abq",
+                             replacement = "ABQ",
+                             x = zoo_print)) %>%
+  mutate(zoo_print = gsub(pattern = "San Antonio", # To fit on plot
+                          replacement = "\nSan Antonio",
+                          x = zoo_print)) %>%
+  mutate(zoo_print = gsub(pattern = "Reid Park",
+                          replacement = "\nReid Park",
+                          x = zoo_print)) %>%
+  mutate(zoo_print = gsub(pattern = "Phx",
+                          replacement = "\nPheonix",
+                          x = zoo_print))
+mutate(zoo_print = gsub(pattern = "San Diego",
+                        replacement = "\nSan Diego",
+                        x = zoo_print))
+mutate(zoo_print = gsub(pattern = "LA",
+                        replacement = "\nLA",
+                        x = zoo_print))
+  
+
+richness_plot <- ggplot(data = sample_values, mapping = aes(x = zoo_print, 
+                                                            y = richness,
+                                                            fill = zoo_print)) +
+  geom_violin() +
+  # geom_boxplot() +
+  ylab("Species Richness") +
+  xlab(element_blank()) +
+  scale_fill_discrete(type = rainbow(length(unique(sample_values$zoo_print)))) +
+  geom_point(data = zoo_values, 
+             mapping = aes(x = zoo_print, y = richness),
+             # color = "black", shape = 17, size = 4.5,
+             shape = 24, 
+             fill = "#FFFFFF",
+             color = "#000000",
+             size = 3,
+             stroke = 0.6) +
+  theme_bw() +
+  theme(legend.position = "none",
+        axis.text = element_text(size = 6), # Font sizes for print
+        axis.title.y = element_text(size = 8),
+        text = element_text(family = "ArialMT"))
+richness_plot
+ggsave(filename = "output/Richness-plot.pdf",
+       plot = richness_plot,
+       width = 5,
+       height = 3,
+       units = "in")
+ggsave(filename = "output/Richness-plot.png",
+       plot = richness_plot,
+       width = 5,
+       height = 3,
+       units = "in")
+
+# Diversity
+diversity_plot <- ggplot(data = sample_values, mapping = aes(x = zoo_print, 
+                                                             y = diversity,
+                                                             fill = zoo_print)) +
+  geom_violin() + # Ahem
+  # geom_boxplot() +
+  ylab("Shannon's Index") +
+  xlab(element_blank()) +
+  scale_fill_discrete(type = rainbow(length(unique(sample_values$zoo_print)))) +
+  geom_point(data = zoo_values, 
+             mapping = aes(x = zoo_print, y = diversity),
+             # color = "black", shape = 17, size = 4.5,
+             shape = 24, 
+             fill = "#FFFFFF",
+             color = "#000000",
+             size = 3,
+             stroke = 0.6) +
+  theme_bw() +
+  theme(legend.position = "none",
+        axis.text = element_text(size = 6), # Font sizes for print
+        axis.title.y = element_text(size = 8),
+        text = element_text(family = "ArialMT"))
+diversity_plot
+ggsave(filename = "output/Diversity-plot.pdf",
+       plot = diversity_plot,
+       width = 5,
+       height = 3,
+       units = "in")
+ggsave(filename = "output/Diversity-plot.png",
+       plot = diversity_plot,
+       width = 5,
+       height = 3,
+       units = "in")
+
+# And some final cleaning up before writing values to file
+zoo_values %>%
+  select(-zoo_print) %>%
+  mutate(diversity = round(diversity, digits = 2)) %>%
+  rename(zoo = zoo_name) %>%
+  mutate(zoo = gsub(pattern = "_",
+                       replacement = " ",
+                       x = zoo)) %>%
+  mutate(zoo = stringr::str_to_title(zoo)) %>%
+  mutate(zoo = gsub(pattern = "Abq",
+                       replacement = "ABQ",
+                       x = zoo)) %>%
+  write.csv(file = "output/perm-tests.csv",
+            row.names = FALSE)
+
+# Pull out values for those permutation t-tests
+richness_t_list <- lapply(X = perm_tests, FUN = "[[", "richness_t")
+diversity_t_list <- lapply(X = perm_tests, FUN = "[[", "diversity_t")
 }
